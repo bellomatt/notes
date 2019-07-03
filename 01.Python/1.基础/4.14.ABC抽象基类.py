650c83b2030f2dd1ba8c6855161a357d0a6c8cf0
holemar <daillow@gmail.com>
2019-07-03 23:41:05 +0800

    抽象属性

--- 01.Python/1.基础/4.14.ABC抽象基类.py
+++ 01.Python/1.基础/4.14.ABC抽象基类.py
@@ -44,11 +44,22 @@
     @six.add_metaclass(abc.ABCMeta)
     class BaseClass(object):
 
-        @abc.abstractproperty # 声明“抽象属性”
+        @abc.abstractproperty # 声明“抽象属性”(只读属性)
         def my_abstract_property(self):
             """
             an abstract property need to be implemented
             """
+
+        # 声明“抽象属性”(读写属性)
+        def get_x(self): pass
+        def set_x(self, value): pass
+        x = abc.abstractproperty(get_x, set_x)
+
+        # 声明“抽象属性”(读写属性)
+        def get_y(self): pass
+        def set_y(self, value): pass
+        y = abc.abstractproperty(get_y, set_y)
+
 
         @abc.abstractmethod # 声明“抽象方法”
         def func_a(self, data):
@@ -65,8 +76,28 @@
     # 派生类，直接继承，要求完全实现抽象基类中的“抽象”内容
     class SubclassImpl(BaseClass):
 
-        # 实现抽象属性
-        my_abstract_property = 5555
+        # 实现抽象属性(只读属性)
+        @property
+        def my_abstract_property(self):
+            return 555
+
+
+        # 实现抽象属性x (只读属性)
+        @property # 申明这是一个属性,同时也定义了取值函数、doc
+        def x(self):
+            '''属性x的doc'''
+            if not hasattr(self, '__x'):
+                self.__x=None
+            return self.__x
+
+        @x.setter # 赋值函数
+        def x(self,value):
+            self.__x=value
+
+
+        # 实现抽象属性y(只读属性)
+        y = 1111
+
 
         # 实现抽象方法
         def func_a(self, data):
@@ -75,6 +106,7 @@
         @staticmethod
         def func_d(c1, data):
             print(type(c1) + str(data))
+
 
     # 虚拟子类， 可以实现抽象基类中的部分API接口，也可以根本不实现
     class RegisteredImpl(object):
