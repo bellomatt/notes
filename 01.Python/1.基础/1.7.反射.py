7136ac41472c8a631fd1d02a93b8dbd97d2db91f
holemar <daillow@gmail.com>
2020-03-05 15:00:14 +0800

    mongodb-时间处理

--- 01.Python/1.基础/1.7.反射.py
+++ 01.Python/1.基础/1.7.反射.py
@@ -83,5 +83,33 @@ callable 函数的用法:
         dump(b) # 打印: <__main__.B instance at 0x01C2F0F8> is callable
         dump(b.method) # 打印: <bound method B.method of <__main__.B instance at 0x01C2F0F8>> is callable
 
+		
+inspect 模块
+	对类，模块的操作，成员，类，模块类型的判断
+	获取源码
+	获取类或函数的参数信息
+	解析堆栈
+
+	inspect.getmembers(object[, predicate])
+	其实现了提取某个对象 object 中的所有成员，以（name,value）对组成的列表返回。
+	其中第二个参数通常可以根据需要调用如下16个方法：
+
+	inspect.ismodule(object)： 是否为模块
+	inspect.isclass(object)：是否为类
+	inspect.ismethod(object)：是否为方法（bound method written in python）
+	inspect.isfunction(object)：是否为函数(python function, including lambda expression)
+	inspect.isgeneratorfunction(object)：是否为python生成器函数
+	inspect.isgenerator(object):是否为生成器
+	inspect.istraceback(object)： 是否为traceback
+	inspect.isframe(object)：是否为frame
+	inspect.iscode(object)：是否为code
+	inspect.isbuiltin(object)：是否为built-in函数或built-in方法
+	inspect.isroutine(object)：是否为用户自定义或者built-in函数或方法
+	inspect.isabstract(object)：是否为抽象基类
+	inspect.ismethoddescriptor(object)：是否为方法标识符
+	inspect.isdatadescriptor(object)：是否为数字标识符，数字标识符有__get__ 和__set__属性； 通常也有__name__和__doc__属性
+	inspect.isgetsetdescriptor(object)：是否为getset descriptor
+	inspect.ismemberdescriptor(object)：是否为member descriptor
+
 
 
